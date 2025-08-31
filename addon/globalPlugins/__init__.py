858c16a1e8f0c6c2975e752ab494a69cf9b2f229
Rafael Santos <rafael.ssantosdev@gmail.com>
2025-08-31 11:59:28 -0300

    Ajuste na inicialização da lib

--- addon/globalPlugins/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-addon_dir = os.path.dirname(os.path.abspath(__file__))
-lib_dir = os.path.join(addon_dir, '..', 'lib')
-lib_dir = os.path.abspath(lib_dir)
-
-if lib_dir not in sys.path:
-    sys.path.insert(0, lib_dir)
--- addon/globalPlugins/describe_image.py
+++ addon/globalPlugins/describe_image.py
@@ -1,7 +1,16 @@
+import sys
+import os
+
+addon_dir = os.path.dirname(os.path.abspath(__file__))
+lib_dir = os.path.join(addon_dir, '..', 'lib')
+lib_dir = os.path.abspath(lib_dir)
+
+if lib_dir not in sys.path:
+    sys.path.insert(0, lib_dir)
+
 import globalPluginHandler
 import speech
 import api
-import os
 import tempfile
 from PIL import ImageGrab
 from stackspot import Stackspot
