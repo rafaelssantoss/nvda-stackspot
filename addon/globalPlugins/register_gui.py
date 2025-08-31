d808dede23d770a4f0ff459f69d47b9426eefe2c
Rafael Santos <rafael.ssantosdev@gmail.com>
2025-08-31 15:26:42 -0300

    correção register_gui.py

--- addon/globalPlugins/register_gui.py
+++ addon/globalPlugins/register_gui.py
@@ -1,14 +1,14 @@
-from gui import StackspotSettingsPanel
-import gui
+from ..gui import StackspotSettingsPanel
+import gui as nvda_gui
 import globalPluginHandler
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def __init__(self):
         super().__init__()
-        gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(StackspotSettingsPanel)
+        nvda_gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(StackspotSettingsPanel)
 
     def terminate(self):
         super().terminate()
-        if StackspotSettingsPanel in gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
-            gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(StackspotSettingsPanel)
+        if StackspotSettingsPanel in nvda_gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
+            nvda_gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(StackspotSettingsPanel)
