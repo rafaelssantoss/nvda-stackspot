90704a0f5d56b664abdec240d9155c7945fe0918
Rafael Santos <rafael.ssantosdev@gmail.com>
2025-08-31 15:35:06 -0300

    correção register_gui.py

--- addon/globalPlugins/register_gui.py
+++ addon/globalPlugins/register_gui.py
@@ -2,23 +2,28 @@ import sys
 import os
 import globalPluginHandler
 
-addon_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if addon_dir not in sys.path:
-    sys.path.insert(0, addon_dir)
+addon_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if addon_root not in sys.path:
+    sys.path.insert(0, addon_root)
 
-from gui import StackspotSettingsPanel
+import importlib.util
+
+gui_path = os.path.join(addon_root, "gui.py")
+spec = importlib.util.spec_from_file_location("addon_gui", gui_path)
+addon_gui = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(addon_gui)
+
+StackspotSettingsPanel = addon_gui.StackspotSettingsPanel
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def __init__(self):
         super().__init__()
-        # Registra o painel de configurações na NVDA
-        import gui as nvda_gui  # módulo gui interno do NVDA
+        import gui as nvda_gui
         nvda_gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(StackspotSettingsPanel)
 
     def terminate(self):
         super().terminate()
         import gui as nvda_gui
-        # Remove o painel ao desativar o addon
         if StackspotSettingsPanel in nvda_gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
             nvda_gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(StackspotSettingsPanel)
