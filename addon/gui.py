eaa88ee6bcce80f82cde827c0f961297e26a7042
Rafael Santos <rafael.ssantosdev@gmail.com>
2025-08-30 21:12:42 -0300

    atualizar gui

--- addon/gui.py
+++ addon/gui.py
@@ -9,19 +9,19 @@ class StackspotSettingsPanel(settings.SettingsPanel):
 
     def makeSettings(self, sizer):
         self.clientIdCtrl = wx.TextCtrl(self, value=addonConfig.getPref("client_id"))
-        sizer.Add(wx.StaticText(self, label="Client ID:"))
+        sizer.Add(wx.StaticText(self, label=_("Client ID:")))
         sizer.Add(self.clientIdCtrl, flag=wx.EXPAND)
 
         self.clientSecretCtrl = wx.TextCtrl(self, value=addonConfig.getPref("client_secret"), style=wx.TE_PASSWORD)
-        sizer.Add(wx.StaticText(self, label="Client Secret:"))
+        sizer.Add(wx.StaticText(self, label=_("Client Secret:")))
         sizer.Add(self.clientSecretCtrl, flag=wx.EXPAND)
 
         self.realmCtrl = wx.TextCtrl(self, value=addonConfig.getPref("realm"))
-        sizer.Add(wx.StaticText(self, label="Realm:"))
+        sizer.Add(wx.StaticText(self, label=_("Realm:")))
         sizer.Add(self.realmCtrl, flag=wx.EXPAND)
 
         self.slugCtrl = wx.TextCtrl(self, value=addonConfig.getPref("slug"))
-        sizer.Add(wx.StaticText(self, label="Slug do Quick Command:"))
+        sizer.Add(wx.StaticText(self, label=_("Slug do Quick Command:")))
         sizer.Add(self.slugCtrl, flag=wx.EXPAND)
 
     def onSave(self):
@@ -32,7 +32,8 @@ class StackspotSettingsPanel(settings.SettingsPanel):
 
 
 def onSettingsDialog():
-    gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(StackspotSettingsPanel)
+    if StackspotSettingsPanel not in gui.settingsDialogs.NVDASettingsDialog.categoryClasses:
+        gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(StackspotSettingsPanel)
 
 
 onSettingsDialog()
